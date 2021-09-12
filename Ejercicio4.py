25d9ebda70249b8f1f6d7b1b0ce428d39a1e1fcb
clja1970 <clja1970@gmail.com>
2021-09-12 19:30:18 +0200

    Modificaciones

--- Ejercicio4.py
+++ Ejercicio4.py
@@ -1,13 +1,18 @@
+
 provincias = ["Almeria","Sevilla", "Cordoba", "Cadiz", "Granada", "Malaga", "Huelva", "Jaen"]
 gentilicios = ["Almeriense","Sevillano", "Cordobee", "Pisha", "Granaino", "Malagueño", "Onubense", "Jienense"]
+for p in provincias:
+    print (p, end =", ")
+    
+respuesta = input ("dime la ciudad de andalucia y te diré el gentilicio: ")
 
-respuesta = input ("Dime la ciudad y te diré el gentilicio: ")
-i=0
+
 EncuentraLaProvincia = False
-
+i=0
 while i <= 7 and not EncuentraLaProvincia:
     EncuentraLaProvincia = provincias [i] == respuesta
     i = i + 1
+    
 if EncuentraLaProvincia:
     print ("El gentilicio es: " + gentilicios [i-1])
 else:
